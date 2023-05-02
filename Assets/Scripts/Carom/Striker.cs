f36e7376667622d79bf23f7fda9d38a9d47130da
NShreedesh <65639214+NShreedesh@users.noreply.github.com>
2023-05-02 15:55:35 +0545

    add wall, bounciness effects, striker shooting

---
+++ Assets/Scripts/Carom/Striker.cs
@@ -0,0 +1,58 @@
+﻿using Scripts.InputControls;
+using UnityEngine;
+
+namespace Scripts.Carom
+{
+    public class Striker : MonoBehaviour
+    {
+        [Header("Required Components")]
+        [SerializeField]
+        private InputController inputController;
+        
+        [Header(("Raycast"))]
+        [SerializeField]
+        private new Camera camera;
+        [SerializeField]
+        private LayerMask strikerLayerMask;
+
+        [Header("Physics")]
+        [SerializeField]
+        private Rigidbody2D rb;
+        [SerializeField]
+        private float shootForce;
+
+        [Header("Shooting Carom")]
+        [SerializeField]
+        private bool isDraggingStriker;
+        [SerializeField]
+        private Vector2 startMousePosition;
+        [SerializeField]
+        private Vector2 endMousePosition;
+
+        private void Update()
+        {
+            Vector3 worldMousePosition = camera.ScreenToWorldPoint(inputController.GetMousePosition());
+            RaycastHit2D hitInfo = Physics2D.Raycast(camera!.transform.position, 
+                worldMousePosition,
+                10, 
+                strikerLayerMask);
+            
+            if (!isDraggingStriker && inputController.GetMousePress().WasPressedThisFrame())
+            {
+                if (hitInfo.collider is null) return;
+                startMousePosition = worldMousePosition;
+                isDraggingStriker = true;
+            }
+            else if (inputController.GetMousePress().IsPressed())
+            {
+                endMousePosition = worldMousePosition;
+            }
+            else if(isDraggingStriker && !inputController.GetMousePress().WasPressedThisFrame())
+            {
+                Vector2 force = endMousePosition - startMousePosition;
+                rb.AddForce(-force * shootForce, ForceMode2D.Impulse);
+                isDraggingStriker = false;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/InputControls/InputController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Scripts.InputControls
+{
+    public class InputController: MonoBehaviour
+    {
+        private Input _input;
+
+        private void Awake()
+        {
+            _input = new Input();
+        }
+
+        private void OnEnable()
+        {
+            _input.Enable();
+        }
+
+        public InputAction GetMousePress() => _input.Carrom.MousePress;
+        
+        public Vector2 GetMousePosition() => _input.Carrom.MousePosition.ReadValue<Vector2>();
+
+        private void OnDisable()
+        {
+            _input.Disable();
+        }
+    }
+}
