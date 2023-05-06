785a24c91fd80ae2ba9a2b2e37d3b30b39b47a43
NShreedesh <65639214+NShreedesh@users.noreply.github.com>
2023-05-06 15:19:39 +0545

    add line renderer to show the shoot power and direction

---
+++ Assets/Scripts/Carom/StrikerArrowDisplay.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace Scripts.Carom
+{
+    public class StrikerArrowDisplay : MonoBehaviour
+    {
+        [Header("Striker")]
+        [SerializeField]
+        private Striker striker;
+
+        [Header("Scale")]
+        [SerializeField]
+        private LineRenderer lineRenderer;
+        [SerializeField]
+        private float scaleUpTo = 3;
+
+        private void Update()
+        {
+            if (!striker.GetIsDragging())
+            {
+                lineRenderer.SetPosition(1, Vector2.zero);
+                return;
+            }
+            
+            Vector2 power = striker.GetPower();
+            lineRenderer.SetPosition(1, power * -scaleUpTo);
+        }
+    }
+}
